c6aa5b047c38facff48bf7da84b2d39d3fbad7fd
alexlamsl <alexlamsl@gmail.com>
2013-09-22 06:37:09 +0100

    multiple calls to next() is now working

--- test/test.js
+++ test/test.js
@@ -8,31 +8,31 @@ var collection = db.getCollection("tests");
 
 exports.testGetCollection = function() {
   assert(collection);
-}
+};
 
 exports.testCollectionNames = function() {
   collection.insert({});
-  assert.notEqual(db.collectionNames().indexOf("tests"), -1)
-}
+  assert.notEqual(db.collectionNames().indexOf("tests"), -1);
+};
 
-exports.testAddUser = function() {
-}
+//exports.testAddUser = function() {
+//}
 
-exports.testAuth = function() {
-}
+//exports.testAuth = function() {
+//}
 
 exports.testDropDatabase = function() {
   collection.insert({});
   db.dropDatabase();
   assert.equal(db.collectionNames().indexOf("tests"), -1);
-}
+};
 
 exports.testEval = function() {
   assert.strictEqual(db.eval("return 42"), 42);
-}
+};
 
-exports.testRemoveUser = function() {
-}
+//exports.testRemoveUser = function() {
+//}
 
 /*
 exports.testRunCommand = function() {
@@ -45,34 +45,34 @@ exports.testRunCommand = function() {
 */
 
 exports.testGetLastErrorObj = function() {
-  assert.equal(db.getLastErrorObj().err, null)
-}
+  assert.equal(db.getLastErrorObj().err, null);
+};
 
 exports.testCollectionCount = function() {
   collection.remove();
   collection.insert({});
   collection.insert({});
   assert(collection.count(), 2);
-}
+};
 
 exports.testDistinct = function() {
   collection.remove();
   collection.insert({name:"John"});
   collection.insert({name:"John"});
   assert.equal(collection.distinct("name", {})[0], "John");
-}
+};
 
 exports.testDrop = function() {
   collection.insert({});
   collection.drop();
   assert.equal(db.collectionNames().indexOf("tests"), -1);
-}
+};
 
 exports.testInsert = function() {
   collection.remove();
   collection.insert({test:"test"});
   assert.equal(collection.find({}).next().test, "test");
-}
+};
 
 exports.testSave = function() {
   collection.remove();
@@ -82,7 +82,7 @@ exports.testSave = function() {
   collection.save(test);
   assert.equal(collection.count(), 1);
   assert.equal(collection.find({}).next().test, "test2");
-}
+};
 
 exports.testUpdate = function() {
   collection.remove();
@@ -92,13 +92,13 @@ exports.testUpdate = function() {
   //NOTE: we don't test all update operators here http://docs.mongodb.org/manual/reference/operator/nav-update/#id1
   assert.equal(collection.count(), 1);
   assert.equal(collection.find({}).next().test, "test2");
-}
+};
 
 exports.testEnsureIndex = function() {
   var indexed = db.getCollection("indexed");
   indexed.ensureIndex({name: 1});
   assert.equal(indexed.getIndexes()[1].key.name, 1);
-}
+};
 
 exports.testFind = function() {
   collection.remove();
@@ -108,7 +108,7 @@ exports.testFind = function() {
   assert.equal(collection.find({result: 2}).next().expression, "1 + 1");
   assert.equal(collection.find({result: 2}, {expression: 1}).next().expression, "1 + 1");
   assert.equal(collection.find({result: 2}, {result: 1}).next().expression, undefined);
-}
+};
 
 exports.testFindOne = function() {
   collection.remove();
@@ -118,8 +118,7 @@ exports.testFindOne = function() {
   assert.equal(collection.findOne({result: 2}).expression, "1 + 1");
   assert.equal(collection.findOne({result: 2}, {expression: 1}).expression, "1 + 1");
   assert.equal(collection.findOne({result: 2}, {result: 1}).expression, undefined);
-
-}
+};
 
 exports.testFindAndModify = function() {
   collection.remove();
@@ -127,16 +126,15 @@ exports.testFindAndModify = function() {
   collection.findAndModify({query: {test:"test"}, update: {$set: {test: "test2"}}});
   assert.equal(collection.find({}).next().test, "test2");
   //TODO test that options work as well
-}
+};
 
 exports.testGetIndexes = function() {
   collection.insert({test:"test"});
   assert.equal(collection.getIndexes()[0].key._id, 1);
-}
+};
 
-exports.testMapReduce = function() {
-
-}
+//exports.testMapReduce = function() {
+//};
 
 exports.testRemove = function() {
   collection.remove();
@@ -146,7 +144,7 @@ exports.testRemove = function() {
 
   assert.equal(collection.count(), 3);
 
-  collection.remove({name:"Smith"})
+  collection.remove({name:"Smith"});
   assert.equal(collection.count(), 2);
 
   //TODO justOne
@@ -155,7 +153,7 @@ exports.testRemove = function() {
 
   collection.remove({name: "John"});
   assert.equal(collection.count(), 0);
-}
+};
 
 exports.testToArray = function() {
   collection.remove();
@@ -167,9 +165,8 @@ exports.testToArray = function() {
   assert.equal(array.length, 3);
   assert.equal(array[0].name, "John");
   assert.equal(array[1].name, "Smith");
-  assert.equal(array[2].name, "Adam")
-
-}
+  assert.equal(array[2].name, "Adam");
+};
 
 exports.testForEach = function() {
   collection.remove();
@@ -185,8 +182,7 @@ exports.testForEach = function() {
   assert.equal(array[0].name, "John");
   assert.equal(array[1].name, "Smith");
   assert.equal(array[2].name, "Adam");
-
-}
+};
 
 exports.testSort = function() {
   collection.remove();
@@ -203,7 +199,7 @@ exports.testSort = function() {
   assert.equal(array[0].name, "Smith");
   assert.equal(array[1].name, "John");
   assert.equal(array[2].name, "Adam");
-}
+};
 
 exports.testLimit = function() {
   collection.remove();
@@ -215,7 +211,7 @@ exports.testLimit = function() {
   assert.equal(array.length, 2);
   assert.equal(array[0].name, "John");
   assert.equal(array[1].name, "Smith");
-}
+};
 
 exports.testSkip = function() {
   collection.remove();
@@ -227,14 +223,14 @@ exports.testSkip = function() {
   assert.equal(array.length, 2);
   assert.equal(array[0].name, "Smith");
   assert.equal(array[1].name, "Adam");
-}
+};
 
 exports.testCount = function() {
   collection.remove();
   collection.insert({});
   collection.insert({});
   assert.equal(collection.find({}).count(), 2);
-}
+};
 
 /*
 exports.testSize = function() {
@@ -274,10 +270,9 @@ exports.testNext = function() {
 
   var cursor = collection.find({});
   assert.equal(cursor.next().name, "John");
-  // NOTE: calingl next more than once breaks, although it looks like it *should* work
-  //assert.equal(cursor.next().name, "Smith");
-  //assert.equal(cursor.next().name, "Adam");
-}
+  assert.equal(cursor.next().name, "Smith");
+  assert.equal(cursor.next().name, "Adam");
+};
 
 if (require.main === module) {
   test.run(exports);
